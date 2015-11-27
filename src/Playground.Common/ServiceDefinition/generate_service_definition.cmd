270d25a13066d678fb366d16f37098efd6a23e95
Mathieu Leenhardt <mathieu.leenhardt@gmail.com>
2015-11-28 00:31:48 +0100

    Added common project, .proto and service definition generator

---
+++ src/Playground.Common/ServiceDefinition/generate_service_definition.cmd
@@ -0,0 +1,2 @@
+cd /d %~dp0
+..\..\packages\Google.Protobuf.3.0.0-alpha4\tools\protoc.exe -I../protos --csharp_out .\ --grpc_out .\ --plugin=protoc-gen-grpc=..\..\packages\Grpc.Tools.0.7.1\tools\grpc_csharp_plugin.exe ../protos/playground_service.proto
